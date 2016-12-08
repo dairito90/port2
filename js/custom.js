96440ca444699fbe86abcec2563666d2008d9053
Dairon Rodriguez <drodriguez@fvi.edu>
2016-12-07 22:09:18 -0500

    changes3

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+$("#page2-button").click(function(e){
+  e.preventDefault();
+  $("#page1")[0].style.display = "none";
+  $("#page2")[0].style.display = "block";
+});
+
+ $("#page1-button").click(function(e){
+   e.preventDefault();
+   $("#page1")[0].style.display = "none";
+   $("#page2")[0].style.display = "block";
+
+ });
