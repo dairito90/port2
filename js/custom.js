f9be013e9a78c4fb2492c4936e2eddf3d943a66d
Dairon Rodriguez <drodriguez@fvi.edu>
2017-02-09 21:09:09 -0500

    changes almost done

--- js/custom.js
+++ js/custom.js
@@ -10,6 +10,11 @@ $("#page1-button").click(function(e){
   $("#page2")[0].style.display = "none";
 });
 
-$('a.portfolio-box-caption').click(function(e){
+$('a.portfolio-box').click(function(e){
     e.preventDefault();
+});
+
+$(".eachProject").click(function(){
+    var aux = $(".eachProject :first-child");
+    window.open(aux.attr("href"), '_blank');
 });
